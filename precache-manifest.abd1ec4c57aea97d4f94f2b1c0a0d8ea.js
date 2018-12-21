4887742af43e047f9216a276a970b7da9fd98b33
sejal <sejal.khatri5@gmail.com>
2018-12-21 06:27:55 -0800

    Updates

---
+++ precache-manifest.abd1ec4c57aea97d4f94f2b1c0a0d8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CodeBreed/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ecdc2e2d531d1251aadc",
+    "url": "/CodeBreed/static/js/runtime~main.ecdc2e2d.js"
+  },
+  {
+    "revision": "ef7f4ce65ba09aa4ce97",
+    "url": "/CodeBreed/static/js/main.ef7f4ce6.chunk.js"
+  },
+  {
+    "revision": "5f759a211d34fcb8ed8c",
+    "url": "/CodeBreed/static/js/1.5f759a21.chunk.js"
+  },
+  {
+    "revision": "ef7f4ce65ba09aa4ce97",
+    "url": "/CodeBreed/static/css/main.9ce5f8c0.chunk.css"
+  },
+  {
+    "revision": "ab7770d1be1a009d69c71a39ee4e10f8",
+    "url": "/CodeBreed/index.html"
+  }
+];
